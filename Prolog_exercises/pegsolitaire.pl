ef2c9c215bf2270f2adfa9657c10380e683974d2
JuandeReiset <grantmyth@Grantmyth96.localdomain>
2019-12-06 15:38:01 -0700

    Added pagoda

--- Prolog_exercises/pegsolitaire.pl
+++ Prolog_exercises/pegsolitaire.pl
@@ -1,16 +1,6 @@
 % Setting up
-/*  % FUll game
 
-Questions:
-depth first search
-1.Representation of board for filled and empty positions
-2. Specification for desired output. WHat is shown step by step?
-every board move
-3. Is the goal included in the initial state? or is it separate?
-4. Explanation of the weighting in Pagoda functions
-*/
- 
-%peg 
+% pegtime = Check the execution time of the program with different boards (NOT AS EFFECTIVE IF DISPLAYING STEP BY STEP)
 
 pegtime(X) :-
   statistics(walltime, [_| []]),
@@ -18,9 +8,24 @@ pegtime(X) :-
   statistics(walltime, [_ | [ExecutionTime]]),
   nl,write('Execution took '), write(ExecutionTime), write(' ms.'), nl.
 
+% UNCOMMENT IN ORDER TO TEST WITH OR WITHOUT INDEPENDENCE AND PAGODA (always comment two of the three).
+/*
+ peg(X):-
+ 1. solitaire_steps(...) Only independence
+ 2. solitaire_steps(...) Brute force
+ 3. solitaire_steps(...) Both pagoda and independence
+*/
+
+% Visualize answer, there exists two methods (always comment one of the two):
+/*
+1. write(X) = prints the list of moves  <---- RECOMMENDED FOR TIME TESTING
+2. displayMoves = shows the execution of the obtained list of moves
+   by applying move to the board step by step    <-- RECOMMENDED FOR STEP BY STEP CHECKING
+*/
 peg(crossbow) :-
  solitaire_steps([31,32,34,35,41,42,43,44,45,53], X,[], [3]),
  %solitaire_steps([31,32,34,35,41,42,43,44,45,53], X, [3]),
+ %solitaire_steps(cross,[31,32,34,35,41,42,43,44,45,53], X,[], [3]),
  display_initialboard([31,32,34,35,41,42,43,44,45,53], [3]),
  nl,
  displayMoves([31,32,34,35,41,42,43,44,45,53],X).
@@ -29,20 +34,36 @@ peg(crossbow) :-
 peg(longbow) :-
  solitaire_steps([20,26,30,31,33,35,36,41,43,45,52,53,54,63], X,[], [3]),
  %solitaire_steps([20,26,30,31,33,35,36,41,43,45,52,53,54,63], X, [3]),
+ %solitaire_steps(long,[20,26,30,31,33,35,36,41,43,45,52,53,54,63], X,[], [3]),
  display_initialboard([20,26,30,31,33,35,36,41,43,45,52,53,54,63], [3]),
   nl,
  displayMoves([20,26,30,31,33,35,36,41,43,45,52,53,54,63],X).
  %write(X).
 
+% notquitedead has no execution without independence or pagoda since it wont return the result in
+% a reasonable amount of time (aka due date)
 peg(notquitedead) :-
- solitaire_steps([2,3,4,12,14,20,21,22,23,24,25,26,30,32,35,36,40,41,42,43,44,45,46,52,54,62,64], X,[], [33]),
+ %solitaire_steps([2,3,4,12,14,20,21,22,23,24,25,26,30,32,35,36,40,41,42,43,44,45,46,52,54,62,64], X,[], [33]),
+  solitaire_steps(nqd,[2,3,4,12,14,20,21,22,23,24,25,26,30,32,35,36,40,41,42,43,44,45,46,52,54,62,64], X,[], [33]),
  display_initialboard([2,3,4,12,14,20,21,22,23,24,25,26,30,32,35,36,40,41,42,43,44,45,46,52,54,62,64], [33]),
  nl,
  displayMoves([2,3,4,12,14,20,21,22,23,24,25,26,30,32,35,36,40,41,42,43,44,45,46,52,54,62,64],X).
  %write(X).
 
+peg(halfdead) :-
+ %solitaire_steps([20,22,23,24,30,34,35,40,41,42,43,44,45,52,54,62,64], X,[], [33]),
+ %solitaire_steps([20,22,23,24,30,34,35,40,41,42,43,44,45,52,54,62,64], X, [33]),
+ solitaire_steps(half,[20,22,23,24,30,34,35,40,41,42,43,44,45,52,54,62,64], X,[], [33]),
+ display_initialboard([20,22,23,24,30,34,35,40,41,42,43,44,45,52,54,62,64], [33]),
+ nl,
+ displayMoves([20,22,23,24,30,34,35,40,41,42,43,44,45,52,54,62,64],X).
+ %write(X).
+
+% same as notquitedead, full takes an unreasonable mount of time to execute withou independe or pagoda
 peg(full) :-
- solitaire_steps([2,3,4,12,13,14,20,21,22,23,24,25,26,30,31,32,34,35,36,40,41,42,43,44,45,46,52,53,54,62,63,64], X,[], [33]),
+ %solitaire_steps([2,3,4,12,13,14,20,21,22,23,24,25,26,30,31,32,34,35,36,40,41,42,43,44,45,46,52,53,54,62,63,64], X,[], [33]),
+ %solitaire_steps([2,3,4,12,13,14,20,21,22,23,24,25,26,30,31,32,34,35,36,40,41,42,43,44,45,46,52,53,54,62,63,64], X, [33]),
+ solitaire_steps(fu,[2,3,4,12,13,14,20,21,22,23,24,25,26,30,31,32,34,35,36,40,41,42,43,44,45,46,52,53,54,62,63,64], X,[], [33]),
  display_initialboard([2,3,4,12,13,14,20,21,22,23,24,25,26,30,31,32,34,35,36,40,41,42,43,44,45,46,52,53,54,62,63,64], [33]),
   nl,
   displayMoves([2,3,4,12,13,14,20,21,22,23,24,25,26,30,31,32,34,35,36,40,41,42,43,44,45,46,52,53,54,62,63,64],X).
@@ -90,28 +111,10 @@ displayMoves(_,[]).
 displayMoves(SB, [Mv | T]) :-
     solitaire_move(SB, Mv, NB),
     display_board(NB),
-    write("Prompt"),nl, get(_),
+    % write("Prompt"),nl, get(_),
     displayMoves(NB, T).  
 
 
-/* INFOR REGARDING THE GAMES
-%Crossbow
-
-crossbow([31,32,34,35,41,42,43,44,45,53]).
-crossgoal([3]).
-
-%Longbow
-longbow([20,26,30,31,33,35,36,41,43,45,52,53,54,63]).
-longgoal([3]).
-
-%Not quite dead
-nqd([2,3,4,12,14,20,21,22,23,24,25,26,30,32,35,36,40,41,42,43,44,45,46,52,54,62,64]).
-nqdgoal([33]).
-
-%full game
-full([2,3,4,12,13,14,20,21,22,23,24,25,26,30,31,32,34,35,36,40,41,42,43,44,45,46,52,53,54,62,63,64]).
-fullgoal([33]).
-*/
 %helper functions
 onboard(N) :- 2 =< N, N =< 4.
 onboard(N) :- 12 =< N, N =< 14.
@@ -162,7 +165,7 @@ solitaire_move(SB, (Start, End), [End|SB2]) :-
     not(member(End,SB2)). % if the space End is empty
 
   
-% TODO implement and add independence and pagoda functions HERE
+% TODO implement and add pagoda functions HERE
 
 % 1. RUN WITHOUT INDEPENDENCE OR PAGODA
 solitaire_steps(GB, [], GB).
@@ -186,10 +189,10 @@ solitaire_steps(Gname,GB, [],_, GB).
 solitaire_steps(Gname,SB, [Mv|Moves],Hist, GB) :-
     solitaire_move(SB, Mv, NewBoard),
     independent(Mv,Hist),
-     /*findall((P,W),(member(P,[simple,...]), %list of pagoda functions
+     findall((P,W),(member(P,[asym,asymR]), %list of pagoda functions
                 wgt(P,NewBoard,W)), Wgts),
-    check_wgts(G,Wgts),
-    */
+    check_wgts(Gname,Wgts),
+    
     solitaire_steps(Gname,NewBoard, Moves,[Mv|Hist], GB).
 /*
 solitaire_steps(G,B,_,[]) :- final_board(G,B).
@@ -204,14 +207,53 @@ solitaire_steps(G,B,Hist,[Mv | Moves]) :-
 */
 
 % Pagoda Values Functions-------------------------------
-pagoda(simple,13,1).
-pagoda(simple,31,1).
-pagoda(simple,33,1).
-pagoda(simple,35,1).
-pagoda(simple,43,1).
 
-goal_wgt(full,simple,1).
-goal_wgt(crossbow,simple,0).
+% asymmectric center weigthing
+pagoda(asym,13,1).
+pagoda(asym,20,-1).
+pagoda(asym,21,1).
+pagoda(asym,23,1).
+pagoda(asym,25,1).
+pagoda(asym,26,-1).
+pagoda(asym,31,2).
+pagoda(asym,33,2).
+pagoda(asym,35,2).
+pagoda(asym,40,-1).
+pagoda(asym,41,1).
+pagoda(asym,43,1).
+pagoda(asym,45,1).
+pagoda(asym,46,-1).
+pagoda(asym,53,1).
+
+% asymmetric center weigthing rotated
+pagoda(asymR,2,-1).
+pagoda(asymR,4,-1).
+pagoda(asymR,12,1).
+pagoda(asymR,13,2).
+pagoda(asymR,14,1).
+pagoda(asymR,31,1).
+pagoda(asymR,32,1).
+pagoda(asymR,33,2).
+pagoda(asymR,34,1).
+pagoda(asymR,35,1).
+pagoda(asymR,52,1).
+pagoda(asymR,53,2).
+pagoda(asymR,54,1).
+pagoda(asymR,62,-1).
+pagoda(asymR,64,-1).
+
+goal_wgt(cross,asym,2).
+goal_wgt(long,asym,2).
+goal_wgt(nqd,asym,2).
+goal_wgt(half,asym,2).
+goal_wgt(fu,asym,2).
+
+goal_wgt(cross,asymR,2).
+goal_wgt(long,asymR,2).
+goal_wgt(nqd,asymR,2).
+goal_wgt(half,asymR,2).
+goal_wgt(fu,asymR,2).
+
 
 % wgt(P, B, Wgt) % -> wgt(+,+,-)
 wgt(_,[], 0).
@@ -242,7 +284,7 @@ overlap((A,B),(C,D)):-
     B = D).
  
 lexorder((A,_),(C,_)):-
-   A =< C.
+   A < C.
 
 
 /*
